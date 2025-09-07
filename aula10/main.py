8992093618d13eb610ae95d5800c587d40fdd313
thepablosantos <iphonepablosantos@gmail.com>
2025-09-07 21:55:38 +0100

    python

---
+++ aula10/main.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um numero: "))
+counter = 0
+
+while numero > counter:
+    print(counter)
+    counter = counter + 1
+
+print("Fim do loop")
---
+++ aula10/main2.py
@@ -0,0 +1,6 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for numero in numeros:
+    print(numero * 10)
+
+print("Fim do loop")
