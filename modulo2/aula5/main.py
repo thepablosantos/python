08883999fd4f3d0e7857678936c90e35be90d952
thepablosantos <iphonepablosantos@gmail.com>
2025-09-08 13:58:11 +0100

    def

---
+++ modulo2/aula5/main.py
@@ -0,0 +1,12 @@
+def soma(x: int, y: int) -> int:
+    return x * y
+
+z = int(input("Digite um numero: "))
+
+if z < 10:
+    print("O numero é menor que 10")
+elif z == 10:
+    print("O numero é igual a 10")
+else:
+    print ("O numero é maior que 10")
+   
